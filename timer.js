9a956fe818561e7427d31fc074ea69155dc8f436
YiLong-Ma <echan241@my.bcit.ca>
2023-05-21 13:41:06 -0700

    update

--- timer.js
+++ timer.js
@@ -2,35 +2,47 @@ let timeLeft = document.getElementById("timeLeft");
 let countdownCircle = document.getElementById('dd');
 let dots = document.querySelector('.dots');
 let endTime = new Date();
-endTime.setMinutes(endTime.getMinutes() + 5);
 
-let x = setInterval(function () {
+let x;
+
+function startTimer() {
+  // Hide the settings container and show the quiz container
+  document.getElementById("settingsContainer").style.display = "none";
+  document.getElementById("quizContainer").style.display = "block";
+
+  // Set the end time for 5 minutes from now
+  endTime = new Date();
+  endTime.setMinutes(endTime.getMinutes() + 5);
+
+  // Start the timer
+  x = setInterval(function() {
     let now = new Date().getTime();
     let distance = endTime - now;
-
 
     let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-
     timeLeft.innerHTML = minutes.toString().padStart(2, '0') + ":" +
-        seconds.toString().padStart(2, '0');
-
+      seconds.toString().padStart(2, '0');
 
     let radius = (distance / (endTime - now)) * 70;
     countdownCircle.setAttribute('r', radius);
 
     if (distance <= 0) {
-        clearInterval(x);
-        timeLeft.innerHTML = "00:00";
-        showAlert();
-
+      clearInterval(x);
+      timeLeft.innerHTML = "00:00";
+      showAlert();
     }
 
     dd.style.strokeDashoffset = 440 - (440 * (distance / (5 * 60 * 1000)));
 
-}, 1000);
+  }, 1000);
+}
 
 function showAlert() {
-    alert("time is over");
+  alert("Time is over");
 }
+
+// Add event listener to the start button
+document.getElementById("start-button").addEventListener("click", startTimer);
+
