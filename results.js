5726502336dc578e29f3bf5f114532beda14b9da
NeemaToto <neematotonchi@gmail.com>
2023-05-21 21:39:30 -0700

    styled results

--- results.js
+++ results.js
@@ -9,31 +9,13 @@ const MAX_HIGH_SCORES = 5;
 
 finalScore.innerText = mostRecentScore;
 
-username.addEventListener('keyup', () => {
-    saveScoreBtn.disabled = !username.value;
-});
-
-saveHighScore = (e) => {
-    e.preventDefault();
-
-    const score = {
-        score: mostRecentScore,
-        name: username.value,
-    };
-    highScores.push(score);
-    highScores.sort((a, b) => b.score - a.score);
-    highScores.splice(5);
-
-    localStorage.setItem('highScores', JSON.stringify(highScores));
-    window.location.assign('/');
-};
 
 const userResponses = JSON.parse(localStorage.getItem('userResponses')); // Retrieve user responses from local storage
 const resultsContainer = document.getElementById('resultsContainer');
 
 if (userResponses && userResponses.length > 0) {
   userResponses.forEach((response) => {
-    const question = document.createElement('p');
+    const question = document.createElement('h4');
     question.textContent = response.question;
     resultsContainer.appendChild(question);
 
